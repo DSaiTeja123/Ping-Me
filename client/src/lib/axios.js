11b70c8b075296b8f8c4ddc696829916dd1280e3
DSaiTeja123 <22341a0539@gmrit.edu.in>
2025-06-07 12:22:52 +0530

    updated axios

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_SERVER_URL;
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
